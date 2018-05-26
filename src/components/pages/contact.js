a7af33031cfd40baa46c0cb47ee1016d8a0e60c8
unknown <puremana@gmail.com>
2018-05-26 21:51:52 +1200

    Contact page styling complete. Needs email js functionality.

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -19,7 +19,21 @@ class Contact extends Component {
     return (
         <div className="contact-page">
             <NavBar page="contact"/>
-            
+            <div className="contact-container">
+                <h2>Contact</h2>
+                <div className="contact-form">
+                    <label for="nameInput">Name</label>
+                    <input type="text" name="nameInput" className="nameInput" id="nameInput" placeholder="Enter name" oninput="handleName()" />
+                    <label for="emailInput">Email</label>
+                    <input type="email" name="emailInput" className="emailInput" id="emailInput" placeholder="Enter your email address" oninput="handleEmail()" />
+                    <label for="messageInput">Message</label>
+                    <textarea class="form-control" name="messageInput" id="messageInput" rows="4" oninput="handleMessage()"></textarea>
+                    <div className="button send" onClick="sendMessage()">
+                        <span className="icon is-small"><i className="fa fa-envelope"></i></span>
+                        Send Message
+                    </div>
+                </div>
+            </div>
         </div>
     );
   }
