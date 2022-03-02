141961da04124f5dccd53bcc7956673a829a7a46
puremana <puremana@gmail.com>
2022-03-03 08:58:25 +1300

    Add freelance developer in experience. Convert links on experience page to rel noopenner no reref

--- src/components/pages/experience.js
+++ src/components/pages/experience.js
@@ -21,6 +21,27 @@ class Experience extends Component {
                 <h2>Experience</h2>
                 
                 <div className="experience-block">
+                    <div className="experience-block-info">
+                        <h3>Freelance Developer</h3>
+                        <p className="italic">April 2021 - Present</p>
+                    </div>
+                    <div className="project-tags">
+                        <div className="tag tag-html">HTML5</div>
+                        <div className="tag tag-css">CSS</div>
+                        <div className="tag tag-js">JS</div>
+                        <div className="tag tag-php">PHP</div>
+                        <div className="tag tag-wordpress">Wordpress</div>
+                        <div className="tag tag-vue">Vue</div>
+                        <div className="tag tag-google-cloud-platform">Google Cloud Platform</div>
+                        <div className="tag tag-nodejs">Node JS</div>
+                        <div className="tag tag-discordjs">Discord JS</div>
+                    </div>
+                    <p className="experience-block-text">
+                        As a freelance developer I have worked on projects across <a href="https://gva.co.nz/" target="_blank" rel="noopener noreferrer">GVA</a>, <a href="https://www.foundationdigital.co.nz/" target="_blank" rel="noopener noreferrer">Foundation Digital</a>, WrathGames, and more. Please contact me for references and project specifics.
+                    </p>
+                </div>
+
+                <div className="experience-block">
                     <a href="https://www.ninetyblack.com/" className="logo-link" target="_blank" rel="noopener noreferrer">
                         <img src="img/svg/ninetyblack-logo.svg" className="experience-block-logo" alt="NinetyBlack Logo" />
                     </a>
@@ -39,7 +60,7 @@ class Experience extends Component {
                         <div className="tag tag-react">React Native</div>
                     </div>
                     <p className="experience-block-text">
-                        At NinetyBlack I worked as a Full Stack Developer to create custom Wordpress sites with unique requirements, maintained internal Laravel projects using <a href="https://asgardcms.com/">Asgard</a> and developed mobile applications with <a href="https://facebook.github.io/react-native/">React Native.</a>
+                        At NinetyBlack I worked as a Full Stack Developer to create custom Wordpress sites with unique requirements, maintained internal Laravel projects using <a href="https://asgardcms.com/" target="_blank" rel="noopener noreferrer">Asgard</a> and developed mobile applications with <a href="https://facebook.github.io/react-native/" target="_blank" rel="noopener noreferrer">React Native.</a>
                     </p>
                 </div>
 
